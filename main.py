089216277468d3f66afedac6b2a70f576f794a07
Xaver115 <xavertv123@gmail.com>
2021-11-02 21:12:22 +0100

    Messaging enabled with clever solution

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from discord_slash import SlashCommand, SlashContext
 import time
 #Own imports
 import utils
+import responses
 
 class BaumBot:
     def __init__(self, token=None):
         self.token = token
 
         self.client = commands.Bot(command_prefix=".")
-        self.slash = SlashCommand(self.client)
+        self.slash = SlashCommand(self.client, sync_commands=True)
+        self.responses = responses.Response()
         self.voice_channel = None
 
         self.init_events()
@@ -23,23 +25,25 @@ class BaumBot:
 
     def init_events(self):
         @self.client.event
-        async def on_ready(): #Show registration data on load-up e.g. 'BaumBot#4721'
+        async def on_ready(): #Show registration data on load-up e.g. 'BaumBot#4721' in console
         	print('We have logged in as {0.user}'.format(self.client))
 
         @self.client.event
         async def on_message(message):
-            pass
+            if message.author == self.client.user:
+            	return
+            await message.channel.send(self.responses.responde(message.content))
 
     def init_commands(self):
-        @self.slash.slash(name="join", description="BaumBot joins the channel of the message author")
+        @self.slash.slash(name="join", description="BaumBot joins the channel of the command author")
         async def join(context: SlashContext):
             self.voice_channel = await utils.check_and_join(self.voice_channel, context)
 
         @self.slash.slash(name="leave", description="BaumBot leaves its current voice channel")
         async def leave(context: SlashContext):
-            await utils.check_and_leave(self.voice_channel, context)
+            await utils.check_and_leave(self.voice_channel)
+            await context.send('I am leaving: "{}"'.format(context.author.voice.channel.name))
             self.voice_channel = None
-            #- Not in any channel
 
         @self.slash.slash(name="test", description="A simple test function")
         async def test(context: SlashContext):
@@ -48,6 +52,7 @@ class BaumBot:
         @self.slash.slash(name="ping", description="Speed Test for the BaumBot")
         async def ping(context: SlashContext):
             await context.send('BaumBot Speed: {}ms'.format(round(self.client.latency * 1000), 0))
+
 
 if __name__ == '__main__':
     token = 'ODkzNTU4MzM5Mjk0ODgzOTEw.YVdNDQ.i-d5auCOMnXh2aH6J5nnxtMvoxs'
---
+++ responses.py
@@ -0,0 +1,23 @@
+class Response:
+    def __init__(self, filepath='documents/responses.txt'):
+        self.filepath = filepath
+        self.anser = ""
+        self.all_responses = {}
+
+        self.read_responses_from_file()
+
+    def read_responses_from_file(self):
+        response_file = open(self.filepath, 'r', encoding='utf-8')
+        lines = response_file.readlines()
+        for line in lines:
+            elements = line.split(' -> ')
+            elements[1] = elements[1].split('\n')[0]
+            self.all_responses[elements[0]] = elements[1]
+
+        print(self.all_responses)
+
+    def responde(self, message):
+        if message not in self.all_responses:
+            print('not in responses')
+            return
+        return self.all_responses[message]
